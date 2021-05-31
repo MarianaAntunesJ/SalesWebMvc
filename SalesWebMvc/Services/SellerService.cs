ed06f10fa2af89d7b7f7da93326ab403615f1e9c
MarianaAntunesJ <marianajantunes97@gmail.com>
2021-05-31 13:24:31 -0300

    Exception handling for delete (referencial integrity)

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -26,9 +26,16 @@ namespace SalesWebMvc.Services
 
         public async Task RemoveAsync(int id)
         {
-            var objeto = await _context.Seller.FindAsync(id);
-            _context.Seller.Remove(objeto);
-            await _context.SaveChangesAsync();
+            try
+            {
+                var objeto = await _context.Seller.FindAsync(id);
+                _context.Seller.Remove(objeto);
+                await _context.SaveChangesAsync();
+            }
+            catch(DbUpdateException e)
+            {
+                throw new IntegrityException("Can't delete seller because he/she has sales");
+            }
         }
 
         public async Task UpdateAsync(Seller seller)
